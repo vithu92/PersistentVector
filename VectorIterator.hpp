dbbcf8c9a90c0f8c1ba4ed9bb3165c8dff21329a
Vithushan Mahendran <vithushan.mahendran@hotmail.com>
2018-12-17 11:04:15 +0100

    second part of the

--- VectorIterator.hpp
+++ VectorIterator.hpp
@@ -13,5 +13,44 @@ template<class T> class VectorIterator {
 public:
 	typedef random_access_iterator_tag iterator_catagory;
 	typedef T value_type;
-	typedef 
+	typedef int _w64 difference_type;
+	typedef T* pointer;
+	typedef T& reference;
+	typedef const T& const_reference;
+	typedef typename PersistentVector<T>::accessor accessor;
+
+	VectorIterator& operator=(const VectorIterator& it) {
+		if (this != &it) {
+			m_vector = it.m_vector;
+			m_pos = it.m_pos;
+		}
+		return *this;
+	}
+
+	// relational Operators
+
+	// increment and decrement operator
+
+	// arithmetische operators
+
+	// access operators
+
+	//schreibrecht
+	access operator*() {
+		return (*m_vector)[m_pos];
+	}
+
+	//ohne schreibrecht
+	T operator*() const {
+		return m_vector[m_pos];
+	}
+
+	//index operator
+	accessor operator[](size_t delta) {
+		return (*m_vector)[m_pos + delta];
+	}
+
+	T operator[](size_t delta) const {
+		return (*m_vector)[m_pos + delta];
+	}
 };
